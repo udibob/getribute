b972ca6aa5b979c7b8f4b3191f858e52ac7a037c
Udi Bobrovsky <udibob@gmail.com>
2013-04-21 08:43:30 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Getribute::Application.config.secret_token = '93146ea464250407dc8100c2306b58f1bd7fc4b556f4d10c25ed448b71bac1057c329e653d0a08c96c97bc76535d8dc545ca238fd47d606794d465169e10f4d9'
